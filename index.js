05c63485f991c236f648f214814867d2e8845395
Tal Amsalem <tamsalem@paloaltonetworks.com>
2022-12-19 09:05:45 +0200

    2gb files

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const fs = require('fs/promises');
 
 const main = async function main (){
-    for (let index = 0; index < 20; index++) {
-        await fs.copyFile( './20MB.zip', `20MB${index}.zip`)        
+    for (let index = 0; index < 100; index++) {
+        await fs.copyFile( './20MB.zip', `20MB-test${index}.zip`)        
     } 
 }
 
