51588c3063cb16919317a6389418f813a804561e
Tal Amsalem <tamsalem@paloaltonetworks.com>
2022-12-19 09:11:16 +0200

    new files

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fs = require('fs/promises');
 
 const main = async function main (){
     for (let index = 0; index < 100; index++) {
-        await fs.copyFile( './20MB.zip', `20MB-test${index}.zip`)        
+        await fs.copyFile( './20MB.zip', `20MB-test${index}.${(Math.random() + 1).toString(36).substring(2)}`)        
     } 
 }
 
